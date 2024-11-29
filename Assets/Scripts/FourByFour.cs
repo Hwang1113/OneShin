572992abad9bc9c09b51d72588fd71887a798fc4
HWANG <jhdsew9471@gmail.com>
2024-11-29 13:51:56 +0900

    NoteUImanager

--- Assets/Scripts/FourByFour.cs
+++ Assets/Scripts/FourByFour.cs
@@ -5,16 +5,22 @@ using UnityEngine.UI;
 public class FourByFour : MonoBehaviour
 {
     [SerializeField]
-    private ONEShin_UIManager UIMg = null;
+    private ONEShin_UINoteManager UIMg = null;
 
+    private float Bpm = 0f; //60bpm 1분에 60번, 1초 1번
+
+    
     private void Awake()
     {
-        UIMg = GetComponentInChildren<ONEShin_UIManager>();
+        UIMg = GetComponentInChildren<ONEShin_UINoteManager>();
     }
+    private void Start()
+    {
+
+    }
+
     private void Update()
     {
         UIMg.MoveNoteToHit();
     }
-
-
 }
