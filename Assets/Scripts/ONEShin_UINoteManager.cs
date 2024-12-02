2f13e1426090c5ab4cd0d1be42e01938d597969b
HWANG <jhdsew9471@gmail.com>
2024-12-02 10:10:58 +0900

    WIP 노트 판정

--- Assets/Scripts/ONEShin_UINoteManager.cs
+++ Assets/Scripts/ONEShin_UINoteManager.cs
@@ -15,17 +15,17 @@ public class ONEShin_UINoteManager : MonoBehaviour
     private Queue<Image> NoteQueue = null; 
     private void Awake()
     {
-        Hitbox = GetComponentInChildren<Image>();
+        Hitbox = GetComponentInChildren<Image>(); 
         HitboxWidth = Hitbox.rectTransform.sizeDelta.x; 
 
         Notebox = Hitbox.GetComponentsInChildren<Image>()[1];
-        NoteboxStartPoint = Notebox.rectTransform.anchoredPosition; // 노트박스가 화면으로 나오기 전 위치
-        NoteBoxEndPoint = Hitbox.rectTransform.anchoredPosition + new Vector2(-HitboxWidth, 0f); // 노트박스가 화면 밖에 나올 위치
-        NoteQueue = new Queue<Image>();
+        NoteboxStartPoint = Notebox.rectTransform.anchoredPosition; // 노트박스가 화면으로 나오기 전 위치 (시작지점)
+        NoteBoxEndPoint = Hitbox.rectTransform.anchoredPosition + new Vector2(-HitboxWidth, 0f); // 노트박스가 화면 밖에 나올 위치 (엔딩지점)
+        NoteQueue = new Queue<Image>(); //노트 담을 큐 리스트
 
     }
     #region private
-    private void MoveNoteToHit()
+    private void MoveNoteToHit()  //////// 노트를 생성하고, 시작 지점(NoteboxStartPoint) 부터 히트박스 뒤(NoteBoxEndPoint) 까지 움직인다.
     {
 
         Image noteGo = Instantiate(Notebox, Hitbox.transform);
@@ -37,6 +37,7 @@ public class ONEShin_UINoteManager : MonoBehaviour
     {
         //Notebox.rectTransform.anchoredPosition = Hitbox.rectTransform.anchoredPosition;
         _Notebox.gameObject.SetActive(true);
+        Stoptiming = 0f;
         float time = 0f;
         while (time < 1f && _Notebox != null)
         {
@@ -52,25 +53,37 @@ public class ONEShin_UINoteManager : MonoBehaviour
         }
         if (_Notebox != null)
         {
+            Stoptiming = 0.5f;
             _Notebox.rectTransform.anchoredPosition = NoteBoxEndPoint;
             HitNote();
         }
+        Stoptiming = 0f;
     }
     #endregion
     #region public
     public void HitNote()
     {
         //Notebox.gameObject.SetActive(false);
-
-        if (NoteQueue.Count > 0 && NoteQueue != null)
+        if (Stoptiming >= 0.5f)
         {
-            Image go = null;
-            if (NoteQueue.TryDequeue(out go))
-                Destroy(go.gameObject);
+            if (NoteQueue.Count > 0 && NoteQueue != null) // 노트갯수가 0초과임과 동시에 null 이 아니면 노트 삭제 
+            {
+                Image go = null;
+                if (NoteQueue.TryDequeue(out go))
+                    Destroy(go.gameObject);
+            }
+            if (NoteQueue.Count >= 0)
+
+                Debug.Log(Stoptiming);
+            //이상 ~ 미만 기준임 
+            // 0.5이하는 무판정  Bad 0.5 ~ 0.6, Good 0.6 ~ 0.75, Perfect 0.75 ~ 0.9, Good 0.9 ~ 1 1이상이 되면 0.5로 자동 치환 그래서 Bad 판정후 노트 사라짐
+
         }
+
+        //Stoptiming 은 점수로 표기하면 되지 않을까?
     }
 
-    public void PushNote()
+    public void PushNote() //노트 생성
     {
         MoveNoteToHit();
     }
