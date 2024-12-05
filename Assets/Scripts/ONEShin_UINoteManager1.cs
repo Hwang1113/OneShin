cec36ec0a8b0d90f6e0be2dfc8096cb5effd4d97
HWANG <jhdsew9471@gmail.com>
2024-12-05 13:06:33 +0900

    디버그 없앰

--- Assets/Scripts/ONEShin_UINoteManager1.cs
+++ Assets/Scripts/ONEShin_UINoteManager1.cs
@@ -257,12 +257,12 @@ public class ONEShin_UINoteManager1 : MonoBehaviour
         while (feverTimeCnt < 3)
         {
             feverTimeCnt += Time.deltaTime;
-            Debug.Log("피버타임!");
+            //Debug.Log("피버타임!");
             yield return null;
         }
         if (feverTimeCnt >= 3)
         {
-            Debug.Log("피버타임끝~");
+            //Debug.Log("피버타임끝~");
             isFever = false;
         }
     }
@@ -270,7 +270,7 @@ public class ONEShin_UINoteManager1 : MonoBehaviour
     public void feverTimeHit() // 피버타임히트 함수 실행하면 1점 추가
     {
             Score += 1;
-        Debug.Log(Score);
+        //Debug.Log(Score);
     }
     #endregion
 
